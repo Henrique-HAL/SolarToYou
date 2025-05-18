18b8945662680426642c9adc217f35f6be99f1aa
Henrique <henriquezzzzzh@gmail.com>
2025-05-17 22:23:33 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/solartoyour/DAO.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.solartoyour;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+/**
+ *
+ * @author Henri
+ */
+public class DAO {
+    public boolean existe(Usuario usuario) throws Exception{
+        String sql= "SELECT*FROM tb_Usuarios WHERE (nome = ? OR email = ?) AND senha = ?"; //Vai verificar nome ou email no Banco de dados//
+        try(Connection conn = ConexaoBD.obtemConexao();
+        PreparedStatement ps = conn.prepareStatement(sql)){
+            
+            ps.setString(1, usuario.getNome()); //Pode ser o Nome 
+            ps.setString(2, usuario.getEmail());// Ou pode ser O email para validar login
+            ps.setString(3, usuario.getSenha());
+            
+            try(ResultSet rs = ps.executeQuery()){
+                return rs.next();
+            }
+            
+        }
+    }
+    
+    
+    
+}
---
+++ src/main/java/com/mycompany/solartoyour/SolarToyour.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.solartoyour;
+
+/**
+ *
+ * @author Henri
+ */
+public class SolarToyour {
+
+    public static void main(String[] args) {
+        
+    }
+}
