138e1e26da5dafa9b5776be5dc9557d46db5de7a
Henrique <henriquezzzzzh@gmail.com>
2025-06-08 10:33:53 -0300

    Mais design

--- src/main/java/CRUDUsuario/TelaPerfil.java
+++ src/main/java/CRUDUsuario/TelaPerfil.java
@@ -42,7 +42,7 @@ public class TelaPerfil extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
         jLabel1.setText("Perfil de Usuário");
 
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagens/TelaLogin.png"))); // NOI18N
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/gif/giftela2.gif"))); // NOI18N
 
         AtualizarjButton.setBackground(new java.awt.Color(255, 153, 0));
         AtualizarjButton.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
